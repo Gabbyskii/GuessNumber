50fa10761beeeb296d8784c12f764d31c7c046e8
Gabbyskii <gkadiga1@gmail.com>
2026-02-16 09:37:30 +0100

    guess game done

--- src/GuessNumber.java
+++ src/GuessNumber.java
@@ -13,16 +13,13 @@ void main(){
     int randomNum = (int)(Math.random() * 11);
     Scanner sc = new Scanner(System.in);
 
-
-
     while (randomNum > 0) {
         System.out.println("Gæt nummer: ");
         int userGuess = sc.nextInt();
 
         if (userGuess == randomNum) {
             System.out.println("Gættet korrekt!" + "\nTallet er: " + randomNum);
-            int userGuess2 = sc.nextInt();
-            System.out.println(userGuess2);
+            break;
         } else if (userGuess > randomNum) {
             System.out.println("Prøv et lavere tal!");
         } else if (userGuess < randomNum) {
